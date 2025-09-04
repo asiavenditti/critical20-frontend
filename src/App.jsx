f8657601b1e1fc04d5b0a07421d939b63d53b072
Your Name <you@example.com>
2025-09-04 11:00:59 +0200

    feat: implement routing and layout structure with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+
+// Pagine
+import HomePage from "./pages/HomePage";
+import ProductListPage from "./pages/ProductListPage";
+import DetailsProductPage from "./pages/DetailsProductPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-
-
   return (
-    <>
-
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route path="/products/:id" element={<DetailsProductPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/pages/ProductListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProductListPage() {
+    return (
+        <div>
+            <h1>ProductList</h1>
+        </div>
+    )
+}

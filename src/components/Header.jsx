8a6a86d265f5dd821739c40f13f8ae2bcc1f142f
Your Name <you@example.com>
2025-09-07 18:01:46 +0200

    Refactor Header component to use NavLink for navigation; implement closeMenu function for mobile menu interactions. Update CSS for active and hover states of navbar links.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
+    const closeMenu = () => {
+        const collapse = document.getElementById('navbarEcommerce');
+        if (collapse && collapse.classList.contains('show')) {
+            collapse.classList.remove('show');
+        }
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bgN fixed-top shadow-sm">
             <div className="container-fluid d-flex align-items-center justify-content-between">
 
-                {/* Spazio vuoto a sinistra solo su desktop per bilanciare il toggle */}
+                {/* Spazio sinistra per bilanciare icone destra su desktop */}
                 <div className="d-none d-lg-block" style={{ width: '100px' }}></div>
 
-                {/* Toggle: visibile solo su sm/md a sinistra */}
+                {/* Toggle per mobile */}
                 <button
                     className="navbar-toggler border-0 d-lg-none me-3"
                     type="button"
@@ -24,32 +31,56 @@ export default function Header() {
                     <FontAwesomeIcon icon={faBars} color="white" />
                 </button>
 
-                {/* Menu collapsable centrato */}
+                {/* Menu centrale */}
                 <div className="collapse navbar-collapse justify-content-center" id="navbarEcommerce">
                     <ul className="navbar-nav mb-2 mb-lg-0 text-center">
                         <li className="nav-item">
-                            <Link className="nav-link nav-custom-link" to="/">Home</Link>
+                            <NavLink
+                                to="/"
+                                className="nav-link"
+                                onClick={closeMenu}
+                            >
+                                Home
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link nav-custom-link" to="/products">Prodotti</Link>
+                            <NavLink
+                                to="/products"
+                                className="nav-link"
+                                onClick={closeMenu}
+                            >
+                                Prodotti
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link nav-custom-link" to="#">Offerte</Link>
+                            <NavLink
+                                to="/offerte"
+                                className="nav-link"
+                                onClick={closeMenu}
+                            >
+                                Offerte
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link nav-custom-link" to="#">Contatti</Link>
+                            <NavLink
+                                to="/contatti"
+                                className="nav-link"
+                                onClick={closeMenu}
+                            >
+                                Contatti
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
 
-                {/* Icone sempre a destra */}
+                {/* Icone a destra */}
                 <div className="d-flex align-items-center gap-3 ms-auto">
-                    <Link to="#" className="icon-link icon-hover-gold" aria-label="Preferiti">
+                    <NavLink to="#" className="icon-link text-light" aria-label="Preferiti">
                         <FontAwesomeIcon icon={faHeart} size="lg" />
-                    </Link>
-                    <Link to="#" className="icon-link icon-hover-gold me-1" aria-label="Carrello">
+                    </NavLink>
+                    <NavLink to="#" className="icon-link text-light me-1" aria-label="Carrello">
                         <FontAwesomeIcon icon={faShoppingCart} size="lg" />
-                    </Link>
+                    </NavLink>
                 </div>
             </div>
         </nav>

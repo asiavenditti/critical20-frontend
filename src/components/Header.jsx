a501fb3e54dbf22657562276febc1434f6583e76
Your Name <you@example.com>
2025-09-10 12:31:01 +0200

    fix Header and ProductListPage components for improved readability and structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBars,
-  faHeart,
-  faShoppingCart,
+    faBars,
+    faHeart,
+    faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import Cart from "./Cart";
-import { width } from "@fortawesome/free-solid-svg-icons/fa0";
 
 // Style
 import "../style/Headerstyle.css";
@@ -20,86 +20,94 @@ export default function Header({ productCart, setProductCart }) {
         if (collapse && collapse.classList.contains("show")) {
             collapse.classList.remove("show");
         }
-    }
+    };
 
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bgN fixed-top shadow-sm text-white">
+                <div className="container-fluid d-flex align-items-center justify-content-between">
+                    <div className="d-none d-lg-block" style={{ width: "100px" }}></div>
 
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bgN fixed-top shadow-sm text-white">
-        <div className="container-fluid d-flex align-items-center justify-content-between">
-          <div className="d-none d-lg-block" style={{ width: "100px" }}></div>
+                    <button
+                        className="navbar-toggler border-0 d-lg-none me-3"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarEcommerce"
+                        aria-controls="navbarEcommerce"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <FontAwesomeIcon icon={faBars} color="white" />
+                    </button>
 
-          <button
-            className="navbar-toggler border-0 d-lg-none me-3"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarEcommerce"
-            aria-controls="navbarEcommerce"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <FontAwesomeIcon icon={faBars} color="white" />
-          </button>
+                    <div
+                        className="collapse navbar-collapse justify-content-center"
+                        id="navbarEcommerce"
+                    >
+                        <ul className="navbar-nav mb-2 mb-lg-0 text-center">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link" onClick={closeMenu}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/products" className="nav-link" onClick={closeMenu}>
+                                    Prodotti
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/offerte" className="nav-link" onClick={closeMenu}>
+                                    Offerte
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/contatti" className="nav-link" onClick={closeMenu}>
+                                    Contatti
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
 
-          <div
-            className="collapse navbar-collapse justify-content-center"
-            id="navbarEcommerce"
-          >
-            <ul className="navbar-nav mb-2 mb-lg-0 text-center">
-              <li className="nav-item">
-                <NavLink to="/" className="nav-link" onClick={closeMenu}>
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to="/products"
-                  className="nav-link"
-                  onClick={closeMenu}
-                >
-                  Prodotti
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/offerte" className="nav-link" onClick={closeMenu}>
-                  Offerte
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to="/contatti"
-                  className="nav-link"
-                  onClick={closeMenu}
-                >
-                  Contatti
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+                    <div className="d-flex align-items-center gap-3 ms-auto">
+                        <NavLink to="#" className="icon-link text-light" aria-label="Preferiti">
+                            <FontAwesomeIcon icon={faHeart} size="lg" />
+                        </NavLink>
 
-          <div className="d-flex align-items-center gap-3 ms-auto">
-            <NavLink
-              to="#"
-              className="icon-link text-light"
-              aria-label="Preferiti"
+                        {/* Bottone Offcanvas */}
+                        <button
+                            className="brt btn btn-link text-light position-relative"
+                            type="button"
+                            data-bs-toggle="offcanvas"
+                            data-bs-target="#offcanvasCart"
+                            aria-controls="offcanvasCart"
+                        >
+                            <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+                            {/* Badge quantità */}
+                            {productCart.length > 0 && (
+                                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                    {productCart.length}
+                                </span>
+                            )}
+                        </button>
+                    </div>
+                </div>
+            </nav>
+
+            {/* Offcanvas del Carrello */}
+            <div
+                className="offcanvas offcanvas-end"
+                style={{ width: "700px" }}
+                tabIndex={-1}
+                id="offcanvasCart"
+                aria-labelledby="offcanvasCartLabel"
             >
-              <FontAwesomeIcon icon={faHeart} size="lg" />
-            </NavLink>
-
-            {/* Pulsante Offcanvas */}
-            <button
-              className="brt"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasCart"
-              aria-controls="offcanvasCart"
-            >
-
                 <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasCartLabel">Il tuo Carrello</h5>
+                    <h5 className="offcanvas-title" id="offcanvasCartLabel">
+                        Il tuo Carrello
+                    </h5>
                     <button
                         type="button"
-                        className="btn-close"
+                        className="btn-close text-reset"
                         data-bs-dismiss="offcanvas"
                         aria-label="Close"
                     />
@@ -110,37 +118,4 @@ export default function Header({ productCart, setProductCart }) {
             </div>
         </>
     );
-
-              <FontAwesomeIcon icon={faShoppingCart} size="lg" />
-            </button>
-          </div>
-        </div>
-      </nav>
-
-      {/* Offcanvas del Carrello */}
-      <div
-        className="offcanvas offcanvas-end"
-        style={{ width: "700px" }}
-        tabIndex={-1}
-        id="offcanvasCart"
-        aria-labelledby="offcanvasCartLabel"
-      >
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasCartLabel">
-            Il tuo Carrello
-          </h5>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          />
-        </div>
-        <div className="offcanvas-body">
-          <Cart />
-        </div>
-      </div>
-    </>
-  );
-
 }

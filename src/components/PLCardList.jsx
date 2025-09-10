76337c639cd9ec2d96ca16d136ab017264485609
marzia <you@example.com>
2025-09-10 11:46:21 +0200

    fix PLCardList

--- src/components/PLCardList.jsx
+++ src/components/PLCardList.jsx
@@ -9,7 +9,7 @@ export default function PLCardLIst({ game }) {
     <div className="card mb-4">
       <div
         key={game.id}
-        className="cardlist card-body text-light d-flex align-items-center justify-content-between"
+        className="cardlist card-body text-dark d-flex align-items-center justify-content-between"
       >
         <img
           className="cardlist-img-mini"
@@ -18,21 +18,33 @@ export default function PLCardLIst({ game }) {
         />
         <div>
           <h6>{game.name}</h6>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">difficoltà</li>
-            <li className="list-group-item">età</li>
-            <li className="list-group-item">numero giocatori</li>
-          </ul>
+          <div className="d-flex list-group-flush">
+            <div className="list-group-item">
+              <span className="badge bg-secondary me-2">
+                Durata: {game.duration}'
+              </span>
+            </div>
+            <div className="list-group-item ml-2">
+              <span className="badge bg-secondary me-2">Età: {game.age}+</span>
+            </div>
+            <div className="list-group-item ml-2">
+              <span className="badge bg-secondary me-2">
+                Giocatori: {game.players}
+              </span>
+            </div>
+            <div className="list-group-item ml-2">
+              <span className="badge bg-success text-dark me-2">
+                {game.difficulty}
+              </span>
+            </div>
+          </div>
         </div>
+
         <Link to={`/products/${game.slug}`} className="btn btn-primary">
+          Vai al gioco
           <FontAwesomeIcon icon={faDice} />
         </Link>
       </div>
     </div>
   );
 }
-/*css appunti
- cardlist-img-mini mi serve per dare all'immagine una dimensione piccolina 
- 
- 
- */
